0bfaa4ae40128568799908e44be4a30a9d4b4e22
borfus <mrpeteyb@gmail.com>
2018-04-09 04:31:08 -0500

    Initial Commit

---
+++ Trigger/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Trigger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnabled = new System.Windows.Forms.Button();
+            this.lblFiring = new System.Windows.Forms.Label();
+            this.prgFiring = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // btnEnabled
+            // 
+            this.btnEnabled.Location = new System.Drawing.Point(12, 12);
+            this.btnEnabled.Name = "btnEnabled";
+            this.btnEnabled.Size = new System.Drawing.Size(95, 27);
+            this.btnEnabled.TabIndex = 0;
+            this.btnEnabled.TabStop = false;
+            this.btnEnabled.Text = "Enable";
+            this.btnEnabled.UseVisualStyleBackColor = true;
+            this.btnEnabled.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEnable_MouseClick);
+            // 
+            // lblFiring
+            // 
+            this.lblFiring.AutoSize = true;
+            this.lblFiring.Location = new System.Drawing.Point(120, 19);
+            this.lblFiring.Name = "lblFiring";
+            this.lblFiring.Size = new System.Drawing.Size(38, 13);
+            this.lblFiring.TabIndex = 1;
+            this.lblFiring.Text = "Firing: ";
+            // 
+            // prgFiring
+            // 
+            this.prgFiring.ForeColor = System.Drawing.Color.Red;
+            this.prgFiring.Location = new System.Drawing.Point(164, 12);
+            this.prgFiring.Name = "prgFiring";
+            this.prgFiring.Size = new System.Drawing.Size(38, 27);
+            this.prgFiring.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prgFiring.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 51);
+            this.Controls.Add(this.prgFiring);
+            this.Controls.Add(this.lblFiring);
+            this.Controls.Add(this.btnEnabled);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Trigger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnabled;
+        private System.Windows.Forms.Label lblFiring;
+        private System.Windows.Forms.ProgressBar prgFiring;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Trigger/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trigger
+{
+    public partial class Form1 : Form
+    {
+        int offsetLocalPlayer;
+        int offsetEntityList;
+        int offsetCrosshairID;
+        int offsetTeam;
+        int offsetHealth;
+        int offsetAttack;
+        int offsetEntityLoopDist;
+
+        int client;
+        string process = "csgo";
+
+        bool enabled = false;
+
+        VAMemory VAM;
+
+        public Form1()
+        {
+            InitializeComponent();
+            VAM = new VAMemory(process);
+        }
+
+        private void btnEnable_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!enabled)
+            {
+                if (GetModuleAddress())
+                {
+                    enabled = true;
+                    btnEnabled.Text = "Disable";
+                    prgFiring.Value = 100;
+                    backgroundWorker1.RunWorkerAsync();
+                }
+                else
+                {
+                    MessageBox.Show("Something went wrong. Try connecting to a game before enabling.", "Error");
+                }
+            }
+            else
+            {
+                if (!backgroundWorker1.IsBusy)
+                {
+                    backgroundWorker1.CancelAsync();
+                }
+                enabled = false;
+                btnEnabled.Text = "Enable";
+                prgFiring.Value = 0;
+            }
+            this.ActiveControl = null;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int forceAttack = client + offsetAttack;
+            int localPlayer = VAM.ReadInt32((IntPtr)(client + offsetLocalPlayer));
+            int myTeam = VAM.ReadInt32((IntPtr)(localPlayer + offsetTeam));
+            int PICPointer;
+            int PICHealth;
+            int PICTeam;
+            int playerInCrosshair;
+
+            while (true && enabled)
+            {
+                playerInCrosshair = VAM.ReadInt32((IntPtr)(localPlayer + offsetCrosshairID));
+                if (playerInCrosshair > 0 && playerInCrosshair < 65)
+                {
+                    PICPointer = VAM.ReadInt32((IntPtr)(client + offsetEntityList + (playerInCrosshair - 1) * offsetEntityLoopDist));
+                    PICHealth = VAM.ReadInt32((IntPtr)(PICPointer + offsetHealth));
+                    PICTeam = VAM.ReadInt32((IntPtr)(PICPointer + offsetTeam));
+
+                    if ((PICTeam != myTeam) && (PICTeam > 1) && (PICHealth > 0))
+                    {
+                        MethodInvoker mi = delegate ()
+                        {
+                            prgFiring.ForeColor = Color.Green;
+                        };
+                        this.Invoke(mi);
+                        VAM.WriteInt32((IntPtr)forceAttack, 5);
+                        Thread.Sleep(1);
+                        VAM.WriteInt32((IntPtr)forceAttack, 4);
+                    }
+                    Thread.Sleep(10);
+                }
+
+                if (prgFiring.ForeColor != Color.Red)
+                {
+                    MethodInvoker mi = delegate ()
+                    {
+                        prgFiring.ForeColor = Color.Red;
+                    };
+                    this.Invoke(mi);
+                }
+            }
+        }
+
+        private bool GetModuleAddress()
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName(process);
+
+                if (processes.Length > 0)
+                {
+                    foreach (ProcessModule m in processes[0].Modules)
+                    {
+                        if (m.ModuleName == "Module Name")
+                        {
+                            client = (int)m.BaseAddress;
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
